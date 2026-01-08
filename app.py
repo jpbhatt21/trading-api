816e7b36da2c15e9628b8ac4a2d40129a06e4f25
Jatan Bhatt <jatanpbhatt@gmail.com>
2026-01-08 22:17:35 +0530

    basic flask template

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify(status='healthy')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
